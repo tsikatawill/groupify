c076684d8407ff106a56ebeac5dba38a5f9075f3
William M. Tsikata <tsikatawill@gmail.com>
2023-02-22 00:09:19 +0000

    add memberPill

---
+++ components/MemberPill.jsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { HiOutlineX } from "react-icons/hi";
+import { Modal } from ".";
+import styles from "./MemberPill.module.css";
+export const MemberPill = ({ member, handleRemove }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const confirmDelete = () => {
+    setShowModal(true);
+  };
+
+  return (
+    <>
+      <div
+        className={clsx(
+          styles.pill,
+          "tooltip p-2 rounded-md hover:[.member-pill-tooltip:opacity:1] bg-slate-200 relative flex gap-2 items-center cursor-pointer text-black"
+        )}
+        onDoubleClick={confirmDelete}
+      >
+        {member}
+
+        <span className="text-red-500 cursor-pointer" onClick={confirmDelete}>
+          <HiOutlineX />
+        </span>
+      </div>
+
+      <Modal show={showModal} handleClose={() => setShowModal(false)}>
+        <h3 className="text-red-500">Delete member?</h3>
+
+        <p className="py-2">
+          <span className="font-bold">{member}</span> will be removed from the
+          group list
+        </p>
+
+        <div className="grid grid-cols-2 gap-4">
+          <button
+            className="btn danger w-full"
+            onClick={() => {
+              handleRemove();
+              setShowModal(false);
+            }}
+          >
+            Delete
+          </button>
+
+          <button
+            className="btn gray w-full\"
+            onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+};
