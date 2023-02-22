9c674004628dd7e81193b8298266e942197ff02c
William M. Tsikata <tsikatawill@gmail.com>
2023-02-22 00:14:53 +0000

    add Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { Layout } from "@/components";
+import { MembersProvider } from "@/context/membersProvider";
+import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MembersProvider>
+      <Layout>
+        <ToastContainer hideProgressBar />
+        <Component {...pageProps} />
+      </Layout>
+    </MembersProvider>
+  );
 }
