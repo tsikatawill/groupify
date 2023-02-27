fb0b335746d2952bedc02173c02fea0deb703c44
William M. Tsikata <tsikatawill@gmail.com>
2023-02-27 15:08:25 +0000

    add animation

--- components/UseCard.jsx
+++ components/UseCard.jsx
@@ -13,11 +13,13 @@ export const UseCard = ({
   ...rest
 }) => {
   const [showing, setShowing] = useState(0);
+  const [animate, setAnimate] = useState(false);
 
   const { title1, title2, desc, image } = caseList[showing];
 
   const handleSkip = useCallback(
     (direction = "next") => {
+      setAnimate(true);
       setShowing((prev) => {
         if (direction !== "next") {
           //   skip backward
@@ -35,12 +37,15 @@ export const UseCard = ({
           }
         }
       });
+      setTimeout(() => setAnimate(false), 1000);
     },
     [caseList]
   );
 
   useEffect(() => {
-    const interval = setInterval(() => handleSkip(), 5000);
+    const interval = setInterval(() => {
+      handleSkip();
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [handleSkip]);
@@ -54,18 +59,25 @@ export const UseCard = ({
               background={background}
               carousel={carousel}
               desc={desc}
+              animate={animate}
               handleSkip={handleSkip}
               title1={title1}
               title2={title2}
             />
 
-            <CardImage image={image} reverse={reverse} />
+            <CardImage
+              animate={animate}
+              carousel={carousel}
+              image={image}
+              reverse={reverse}
+            />
           </>
         ) : (
           <>
-            <CardImage image={image} />
+            <CardImage animate={animate} carousel={carousel} image={image} />
 
             <CardText
+              animate={animate}
               background={background}
               carousel={carousel}
               desc={desc}
@@ -81,8 +93,14 @@ export const UseCard = ({
   );
 };
 
-const CardImage = ({ image, reverse }) => (
-  <div className={clsx("image ", reverse && "row-start-1 md:col-start-2")}>
+const CardImage = ({ image, reverse, animate, carousel = false }) => (
+  <div
+    className={clsx(
+      "image",
+      carousel && animate && "animate-appear",
+      reverse && "row-start-1 md:col-start-2"
+    )}
+  >
     <div className="wrapper m-auto w-full rounded-md overflow-hidden shadow-md shadow-slate-800 border border-slate-500 hover:border-slate-400 hover:shadow-lg transition-all duration-300 ease-out">
       <Image
         className="w-full h-full"
@@ -101,21 +119,29 @@ const CardText = ({
   carousel,
   desc,
   handleSkip,
+  animate,
   background = "/images/one.png",
 }) => (
   <div
-    className="text flex flex-col items-center  md:items-start text-center md:text-start bg-no-repeat bg-contain bg-center"
+    className={clsx(
+      "text flex flex-col items-center  md:items-start text-center md:text-start bg-no-repeat bg-contain bg-center"
+    )}
     style={{ backgroundImage: `url(${background})` }}
   >
     <h3 className="text-cyan-500">{title1}</h3>
 
-    <div>
+    <div className={carousel && animate && "animate-appear md:animate-slide"}>
       <h4>{title2}</h4>
       <p className="text-slate-300 max-w-sm md:w-full">{desc}</p>
     </div>
 
     {carousel && (
-      <div className="flex gap-2">
+      <div
+        className={clsx(
+          "flex gap-2",
+          animate && "animate-appear md:animate-slide"
+        )}
+      >
         <button className="btn dark px-3" onClick={() => handleSkip("prev")}>
           <HiArrowLeft />
         </button>
