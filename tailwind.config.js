4e98160649d949c7a097199096b58ecce1e2c4e7
William M. Tsikata <tsikatawill@gmail.com>
2023-02-27 15:05:49 +0000

    add custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        appear: "appear 1s ease-out",
+        slide: "slide 0.8s ease-out",
+      },
+      keyframes: {
+        appear: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: "100%" },
+        },
+        slide: {
+          "0%": { transform: "translateX(50px)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
