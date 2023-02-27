4bf663c05741e9e31f3c4ce8418a9292d18f5381
William M. Tsikata <tsikatawill@gmail.com>
2023-02-27 15:08:01 +0000

    add grid cols for grouped x ungroupd members

--- components/MemberGrid.jsx
+++ components/MemberGrid.jsx
@@ -12,7 +12,14 @@ export const MemberGrid = ({ members, grouped = false }) => {
   };
   return (
     <ul className={clsx("relative p-5 rounded-md", !grouped && "bg-slate-800")}>
-      <div className="scroll-container flex flex-wrap custom-scrollbar gap-5 max-h-56 items-start justify-center overflow-y-scroll overflow-x-hidden hidden-scrollbar">
+      <div
+        className={clsx(
+          "scroll-container grid grid-cols-1 items-start custom-scrollbar gap-5 max-h-56 justify-center overflow-y-scroll overflow-x-hidden hidden-scrollbar",
+          grouped
+            ? "sm:grid-cols-2 md:grid-cols-1"
+            : "sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        )}
+      >
         {members.map((member, idx) => (
           <MemberPill
             key={idx}
